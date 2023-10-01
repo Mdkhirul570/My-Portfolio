fea02f841a3bcf2332e09eff5a985bd12c6dc0e2
sabrin29 <khirulaust@gmail.com>
2023-10-01 09:50:26 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// aos initialize
+$(document).ready(function(){
+      AOS.init();
+    });
+
+    var typed = new Typed('.typedjs', {
+      strings: ['©2023 UI Portfolio. All Rights Reserved | Design by Khirul', '©2023 UI Portfolio. All Rights Reserved | Design by Khirul'],
+      loop: true,
+      startDelay: 1000,
+      backSpeed: 100,
+      backDelay: 1000,
+      typeSpeed: 100,
+      showCursor: false,
+  });
